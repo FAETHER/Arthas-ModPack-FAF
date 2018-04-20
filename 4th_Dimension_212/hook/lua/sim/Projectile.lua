feb33bfea389047dff162b41208c67f2f90c2b46
Kyryl Melekhin <k.melekhin@gmail.com>
2018-04-20 19:31:48 -0400

    Nukes

--- 4th_Dimension_212/hook/lua/sim/Projectile.lua
+++ 4th_Dimension_212/hook/lua/sim/Projectile.lua
@@ -3,33 +3,6 @@ do
 local OldProjectile = Projectile
 Projectile = Class(OldProjectile) {
 
-    DoDamage = function(self, instigator, damageData, targetEntity)
-        local damage = damageData.DamageAmount
-        if damage and damage > 0 then
-            local radius = damageData.DamageRadius
-            if radius and radius > 0 then
-                if not damageData.DoTTime or damageData.DoTTime <= 0 then
-                    DamageArea(instigator, self:GetPosition(), radius * 0.5, damage * 0.4, damageData.DamageType, damageData.DamageFriendly, damageData.DamageSelf or false)
-                    DamageArea(instigator, self:GetPosition(), radius * 0.75, damage * 0.3, damageData.DamageType, damageData.DamageFriendly, damageData.DamageSelf or false)
-		            DamageArea(instigator, self:GetPosition(), radius, damage * 0.3, damageData.DamageType, damageData.DamageFriendly, damageData.DamageSelf or false)
-                 	if radius > 1.4 then
-        				DamageArea(instigator, self:GetPosition(), radius* 0.7, 1, 'Force', true)
-        				DamageArea(instigator, self:GetPosition(), radius * 0.7, 1, 'Force', false)
-		            end
-		            else
-                    ForkThread(DefaultDamage.AreaDoTThread, instigator, self:GetPosition(), damageData.DoTPulses or 1, (damageData.DoTTime / (damageData.DoTPulses or 1)), radius, damage, damageData.DamageType, damageData.DamageFriendly)
-                end
-            #ONLY DO DAMAGE IF THERE IS DAMAGE DATA.  SOME PROJECTILES DO NOT DO DAMAGE WHEN THEY IMPACT.
-            elseif damageData.DamageAmount and targetEntity then
-                if not damageData.DoTTime or damageData.DoTTime <= 0 then
-                    Damage(instigator, self:GetPosition(), targetEntity, damageData.DamageAmount, damageData.DamageType)
-                else
-                    ForkThread(DefaultDamage.UnitDoTThread, instigator, targetEntity, damageData.DoTPulses or 1, (damageData.DoTTime / (damageData.DoTPulses or 1)), damage, damageData.DamageType, damageData.DamageFriendly)
-                end
-            end
-        end
-    end,
-
     DoTaperedDamage = function(self, instigator, damageRadius, damage, damageType, damageFriendly, damageSelf, EdgeDamageRatio)
         if damageRadius and damage then
             local Run1Step = false
